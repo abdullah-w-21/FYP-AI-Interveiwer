53e65346115018d30e57cf7bc192f75e403576fa
Abdullah Wasim <81172855+abdullah-w-21@users.noreply.github.com>
2024-08-25 07:35:27 -0700

    Delete fyp directory

--- fyp/main.py
+++
@@ -1,9 +0,0 @@
-import interviewbot
-from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import CountVectorizer
-import json
-import joblib
-
-if __name__ == "__main__":
-    interviewbot.interview_bot()
-
